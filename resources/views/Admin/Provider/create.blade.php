68c0bc1cfbc6c93c8a447cc68ebd48be33a8c25c
ibtissam <ibtissamelhani36@gmail.com>
2024-04-17 20:09:14 +0100

    fix activities in admin

--- resources/views/Admin/Provider/create.blade.php
+++ resources/views/Admin/Provider/create.blade.php
@@ -57,7 +57,7 @@
 
             <div>
                 <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Password</label>
-                <input type="password" placeholder="Enter your password" name="password"
+                <input type="text" placeholder="Enter password" name="password"
                     class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                 @error('password')
                     <p class="text-sm text-red-500">{{ $message }}</p>
